8ee7781355f9a8cac3051351739e5f96bfc19635
sushant1305 <sushant.mad@gmail.com>
2017-06-08 12:27:06 +0530

    python test

---
+++ sushant.py.test/src/subtract_dates.py
@@ -0,0 +1,17 @@
+#Subtract two dates
+import datetime
+
+def hello():
+    print "Welcome to the seconds calculator"
+    day = raw_input("Enter your birth date: ")
+    month = raw_input("Enter your birth month: ")
+    year = raw_input("Enter your birth year: ")
+    
+    dob = day + : + month + : + year
+    print "Your date of birth is %s/%s/%s" %(day,month,year)
+    
+    number of seconds you have lived is 
+
+
+
+hello()
